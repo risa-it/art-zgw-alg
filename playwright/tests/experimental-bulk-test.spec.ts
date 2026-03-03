2d1c458eda40a3156c10cc281de5e481b166e60a
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-03-03 17:06:12 +0100

    Bundle happy-flow tetst into single file to simplify execution ahead of automation.

---
+++ playwright/tests/experimental-bulk-test.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { loginAsAdminLocal } from './helpers/LoginAsAdminLocal';
+import { startNewKccDossier } from './helpers/StartNewKccDossier';
+import { setCLientStatusToAnonymous } from './helpers/SetClientStatusToAnonymous';
+import { fillOutQuestionFormHandledByOperator } from './helpers/FillOutQuestionFormHAndledByOperator';
+import { retrieveResidentInfoByBsn } from './helpers/RetrieveResidentInfoByBsn';
+import { manuallyConfirmResidentInfo } from './helpers/ManuallyConfirmResidentInfo';
+import { fillOutQuestionFormSendQuestion } from './helpers/FillOutQuestionFormSendQuestion';
+
+test.describe('happyflow-bulk', () => {
+
+  test('happyflow-anonieme-vraag', async ({ page }) => {
+    await loginAsAdminLocal(page);
+    await startNewKccDossier(page);
+    await setCLientStatusToAnonymous(page)
+    await fillOutQuestionFormHandledByOperator(page)
+  });
+
+  test('happyflow-inwoner-vraag.spec', async ({ page }) => {
+    await loginAsAdminLocal(page);
+    await startNewKccDossier(page);
+    await retrieveResidentInfoByBsn(page)
+    await manuallyConfirmResidentInfo(page)
+    await fillOutQuestionFormSendQuestion(page)
+  });
+
+});
