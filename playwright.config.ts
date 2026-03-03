7e0c957b4c09d9636aaa86e178628d64f8ecf3b4
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-03-03 13:41:44 +0100

    add playwright config file

---
+++ playwright.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './playwright/tests',
+  testMatch: ['**/*.spec.ts'],
+  outputDir: './test-results',
+});
