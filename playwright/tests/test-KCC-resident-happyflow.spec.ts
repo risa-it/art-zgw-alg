bfd5f2b813124819fce7893fdfff0195c953e2de
JanelTT <177222031+Janel-RisaIT@users.noreply.github.com>
2026-03-12 16:00:13 +0100

    fix spacing

--- playwright/tests/test-KCC-resident-happyflow.spec.ts
+++ playwright/tests/test-KCC-resident-happyflow.spec.ts
@@ -10,27 +10,27 @@ import {fillOutQuestionFormSendQuestionWithSecondQuestion} from './helpers/KCC-7
 test.describe("workflow-kcc-resident-test", () => {
 
 test.beforeEach(async ({page}) => {
-       await loginAndCreateKccDossier(page);  
-    });
+  await loginAndCreateKccDossier(page);  
+});
 
 test ('login, create new dossier, find resident info with BSN, fill in question form, send question', async ({ page })=>{
-    await residentInfoByBsn(page);
-    await fillOutQuestionFormSendQuestion(page);
+  await residentInfoByBsn(page);
+  await fillOutQuestionFormSendQuestion(page);
 });
 
 test ('login, create new dossier, find resident info by name and birth date, fill in question form, send question', async ({ page })=>{
-    await residentInfoByNameAndBirthDate(page);
-    await fillOutQuestionFormSendQuestion(page);
+  await residentInfoByNameAndBirthDate(page);
+  await fillOutQuestionFormSendQuestion(page);
 });
 
 test ('login, create new dossier, find resident info by postalcode and house number, fill in question form, send question', async ({ page })=>{
-    await residentInfoByPostalCodeAndHouseNumber(page);
-    await fillOutQuestionFormSendQuestion(page);
+  await residentInfoByPostalCodeAndHouseNumber(page);
+  await fillOutQuestionFormSendQuestion(page);
 });
 
 test ('login, create new dossier, add resident info, fill in question, add a second question', async ({ page })=>{
-    await residentInfoByPostalCodeAndHouseNumber(page);
-    await fillOutQuestionFormSendQuestionWithSecondQuestion(page);
+  await residentInfoByPostalCodeAndHouseNumber(page);
+  await fillOutQuestionFormSendQuestionWithSecondQuestion(page);
 });
 
 });
